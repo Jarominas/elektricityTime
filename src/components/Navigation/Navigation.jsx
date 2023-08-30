4a91fa559c211c09752c3d47ec37a8f220ac52df
Artur Jarominas <artur.jarominas@gmail.com>
2023-08-30 20:06:11 +0300

    nav text

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,18 +3,14 @@ import logo from '/src/logo.svg'
 
 const Navigation = () => {
       return (
-            <Navbar className='bg-body-tertiary'>
+            <Navbar className='bg-body-tertiary m-2'>
                   <Container>
                         <Navbar.Brand href='#home'>
-                              <img src={logo} alt='' width={30} height={30} />
-                              Navbar with text
+                              <img src={logo} alt='' width={40} height={40} />
+                              Electricity/Gas Prices
                         </Navbar.Brand>
                         <Navbar.Toggle />
-                        <Navbar.Collapse className='justify-content-end'>
-                              <Navbar.Text>
-                                    Signed in as: <a href='#login'>Mark Otto</a>
-                              </Navbar.Text>
-                        </Navbar.Collapse>
+                        <Navbar.Collapse className='justify-content-end'></Navbar.Collapse>
                   </Container>
             </Navbar>
       )
