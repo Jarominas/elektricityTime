3041c6c4690ba973ee413766df59c7836124fdef
Artur Jarominas <artur.jarominas@gmail.com>
2023-09-07 20:43:19 +0300

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import Navigation from './components/Navigation/Navigation'
 import Body from './components/Body/Body'
 import Footer from './components/Footer'
 import ModalError from './ModalError'
-
+import { Routes, Route } from 'react-router-dom'
+import Contact from './components/Contacts'
+import useGetData from './components/effects/useGetData'
 function App() {
+      useGetData()
+
+      const mainPage = (
+            <>
+                  <Body />
+                  <Footer />
+            </>
+      )
       return (
             <>
                   <Container>
                         <Navigation />
-                        <Body />
-                        <Footer />
+                        <Routes>
+                              <Route path='/' element={mainPage} />
+                              <Route path='/gas' element={mainPage} />
+                              <Route path='/gas/:dataType' element={mainPage} />
+                              <Route path='/electricity' element={mainPage} />
+                              <Route path='/contacts' element={<Contact />} />
+                        </Routes>
                         <ModalError />
                   </Container>
             </>
